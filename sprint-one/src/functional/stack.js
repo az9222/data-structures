821ed726d4e617328f90af0c1e14d257da329af3
Angela Zhou <angelazhou922@gmail.com>
2018-09-19 11:44:20 -0700

    Passed stack tests

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,22 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var count = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    count++;
   };
 
   someInstance.pop = function() {
+    count--;
+    if (count < 0) {
+      count = 0;;
+    }
   };
 
   someInstance.size = function() {
+    return count;
   };
 
   return someInstance;
