30455122dbc794980d6cd661902bf0fad06bfb3a
Angela Zhou <angelazhou922@gmail.com>
2018-09-19 15:44:12 -0700

    queue works

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,40 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var obj = {};
+  obj.storage = {};
+  obj.enqueueCount = 0;
+  obj.dequeueCount = 0;
+  extend(obj, queueMethods);
+  return obj;
+}
+
+var extend = function(obj, methods) {
+  for (var key in methods) {
+    obj[key] = methods[key];
+  }
+}
+
+var queueMethods = {
+  enqueue: function(value){
+    this.enqueueCount++;
+    this.storage[this.enqueueCount] = value;
+    let arr = Object.values(this.storage);
+    return arr[this.enqueueCount];
+  },
+
+  dequeue: function() {
+     if (this.enqueueCount < 0) {
+      count = 0;
+    }
+    var arr = Object.values(this.storage);
+    var returnValue = arr[this.dequeueCount];
+    this.dequeueCount++;
+    return returnValue
+  },
+
+  size: function(){
+    if (this.dequeueCount > this.enqueueCount) {
+      return 0;
+    }
+    return this.enqueueCount - this.dequeueCount;
+  }
 };
-
-var queueMethods = {};
-
-
