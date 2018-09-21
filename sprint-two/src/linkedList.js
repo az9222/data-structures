a84734678be8c779dfa7177d0be4938d0499d39a
Angela Zhou <angelazhou922@gmail.com>
2018-09-20 18:25:24 -0700

    progress

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,20 +2,29 @@ var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
+  list.length = 0;
 
   var counter = 0;
   list[counter]; //keeps track of length
 
   list.addToTail = function(value) {
-    list.tail = new Node(value);
-    counter++; //increments length as we push
+    this.tail = new Node(value);
+    // counter++; //increments length as we push
+    this.length++;
+    return this;
   };
 
   list.removeHead = function() {
     var currenthead = this.head;
     this.head = this.head.next;
-    // counter--;
+    this.length--;
     return currenthead;
+    
+    //maybe: if there are no nodes return und
+    //store current head property in variable
+    //set head property to be current heads next property
+    //decrement length by 1
+    //return removed ndoe
   };
 
   list.contains = function(target) {
