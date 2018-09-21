4f872f5b8c99de246a4b68c64a5e2d400da21607
Angela Zhou <angelazhou922@gmail.com>
2018-09-21 10:53:10 -0700

    tree completed

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,19 +1,44 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-
-  // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = []; 
+  extend(newTree, treeMethods); 
   return newTree;
 };
+
+function extend (obj, objMethods) {
+  for (var key in objMethods) {
+    obj[key] = objMethods[key];
+  }
+}
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  //create a new tree
+  var tree = new Tree(value);
+  //push it to the children array
+  this.children.push(tree);
 };
 
 treeMethods.contains = function(target) {
+  var containsTarget = false;
+  function recurse(tree, target) {
+    //if root (this.value) equals to target returns true
+    if (tree.value === target) {
+      containsTarget = true;
+      return containsTarget;
+    } else {
+    //if not, for loop through the children to see if children value equals to target
+      for (var i = 0; i < tree.children.length; i++) {
+        //if children value match target, return true
+        var child = tree.children[i]
+        recurse(child, target);
+      }
+    }
+  }
+  recurse(this, target);
+  return containsTarget;
 };
 
 
