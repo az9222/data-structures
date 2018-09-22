c0ca3aae0d29bc62bb83b909dc84a666e07d0a0e
Angela Zhou <angelazhou922@gmail.com>
2018-09-21 23:19:50 -0700

    binary tree update

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,56 +1,48 @@
 //make a new node Class
 //left = null, right = null , value = value
-class Node {
-    constructor(value) {
-        this.left = null;
-        this.right = null;
-        this.value = value;
-    }
-}
-
 class BinarySearchTree {
     //this.root goes in the constructor
-    constructor() {
-        this.root = null; //<- come back to this
+    constructor(value) {
+        this.value = value; //start root at value they give you
+        this.left = null;
+        this.right = null;
     }
 
     insert(value) {
-        var inputNode = new Node(value);
-        //if there is no root
-        if (this.root === null) {
-            //new input value (new Node) becomes the root
-            this.root = inputNode;
-            //return this
-            return this;
-        }
-
-        //while there is a root
-        while (this.root) {
-            //if there are duplicates return und
-            if (value === this.root.value) {
-                return undefined;
-            }
+        var inputNode = new BinarySearchTree(value);
+        //keep track of the value of the root node
+        //while there is a node
+        //if there are duplicates return und
+        if (value === this.value) {
+            return undefined;
+        } else if (value < this.value) { 
             //if value < root
-            if (value < this.root.value) { //<-this.root.value???
-                //if the root has no left
-                if (this.root.left === null) {
-                    //the newNode becomes the new left
-                    this.root.left = inputNode;
-                    return this;
-                }
+            //if the root has no left
+            if (this.left === null) {
+                //the newNode becomes the new left
+                this.left = inputNode;
+                return this;
+            } else {
                 //if the root has a left, the root becomes the
-                this.root = this.root.left;
-                } else if (value > this.root.value) {
-                    if (this.root.right === null) {
-                        this.root.right = inputNode;
-                        return this;
-                    }
-                    this.root = this.root.right;
-                }
+                this.left.insert(value)
             }
-        }
-        
-    //contains
+        } else if (value > this.value) {
+            if (this.right === null) {
+                this.right = inputNode;
+                return this;
+            } else {
+                this.right.insert(value)
+            }
+        }     
+    }
+
+    contains() {
+
+    }
+    
+    depthFirstLog() {
+
+    }
 };
 
 /*
--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,12 +1,21 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {}; 
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  //loop through set properties
+  for (var key in this.set) {
+    //if the set does not contain the item, add the item
+    if (this.set.key === undefined) {
+      this.set[key] = item;
+    }
+  }
+  //return the set
+  return set;
 };
 
 setPrototype.contains = function(item) {
