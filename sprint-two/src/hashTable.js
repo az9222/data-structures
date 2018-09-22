f87d984f06917c2b2bff4d2bef5e828f1eec2025
Angela Zhou <angelazhou922@gmail.com>
2018-09-21 19:22:04 -0700

    hsah table

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,5 +1,4 @@
 
-
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
@@ -13,10 +12,28 @@ HashTable.prototype.insert = function(k, v) {
   }
   //push k, v in an array at the index
   this._storage[index].push([k,v]);
+  //overwrite duplicate keys
+  for (var i = 0; i < this._storage[index].length; i++) {
+    if (this._storage[index][i][0] === k) {
+      return this._storage[index][i][1];
+    }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  //if the bucket is empty return undefined
+  if (!this._storage[index]) {
+    return undefined;
+  }
+  //if not, loop through the nested arrays and check if the k matches the first element of the sub array
+  for (var i = 0; i < this._storage[index].length; i++) {
+    if (this._storage[index][i][0] === k) {
+      return this._storage[index][i][1];
+    }
+  }
+  return undefined;
+  // return this;
+  //return subarray
 };
 
 HashTable.prototype.remove = function(k) {
@@ -29,4 +46,16 @@ HashTable.prototype.remove = function(k) {
  * Complexity: What is the time complexity of the above functions?
  */
 
+ /*
+ ********** NOTE: **********
+ * Do not edit this code unless you see a bug!
+ */
+
+
+// This class represents an array with limited functionality and a maximum size.
+// It will ensure that you don't accidentally try to use up too much space.
+//
+// Usage:
+//   limitedArray.set(3, 'hi');
+//   limitedArray.get(3); // returns 'hi'
 
